64ebcb941e2f2ad93b02b0f6b754453ce7fd910d
Denis Vasilev <lyon_den@mail.ru>
2023-02-06 16:47:32 +0300

    initial refactoring

---
+++ TaskListRealm/App/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  TaskListRealm
+//
+//  Created by Денис Васильев on 06.02.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
